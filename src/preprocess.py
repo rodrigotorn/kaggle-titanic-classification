a47dfcccebade1f7e13b0089ac5f4213b4641feb
Rodrigo Torn <rodrigo.torn@outlook.com>
2022-01-30 22:09:21 -0300

    refactor: Improve preprocessing module

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,45 +4,70 @@ import pandas as pd
 import numpy as np
 
 
-def _fill_nan(series: pd.Series) -> pd.Series:
-  if series.dtype is np.dtype(object):
-    return series.fillna(series.mode())
-  else:
-    return series.fillna(series.mean())
-
-
 class Preprocess():
   """Preprocess class. Scaler is defined when instantiated"""
   def __init__(
     self,
-    train_df: pd.DataFrame,
-    scaler,
+    scaler=None,
   ) -> None:
-    self._scaler = scaler.fit(self.clean(train_df))
+    self.scaler = scaler if scaler else None
 
-  def clean(
-    self,
-    df: pd.DataFrame,
-  ) -> pd.DataFrame:
 
-    df = df[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
-    df = df.apply(_fill_nan, axis=0)
+  def transform(self, train_df, test_df):
+    transformed_df = []
+    for df in [train_df, test_df]:
+      df = df[[
+        'Pclass',
+        'Sex',
+        'Age',
+        'SibSp',
+        'Parch',
+        'Fare',
+        'Embarked'
+      ]]
 
-    pclass_dummy = pd.get_dummies(df['Pclass'])
-    pclass_dummy.rename(
-      columns={1:'Pclass1', 2:'Pclass2', 3:'Pclass3'},
-      inplace=True
+      df['Age'] = df['Age'].fillna(
+        df['Age'].mean()
+      )
+      df['Embarked'] = df['Embarked'].fillna(
+        df['Embarked'].mode()[0]
+      )
+      
+      df['Fare'] = df['Fare'].map(
+        lambda i: np.log(i) if i > 0 else 0
+      )
+
+      categorical = ['Pclass', 'Sex', 'Embarked']
+
+      for col in categorical:
+        dummy = pd.get_dummies(df[col], prefix=col)
+        df = pd.concat([df, dummy], axis=1)
+        df.drop(col, axis=1, inplace=True)
+      
+      df.drop(
+        labels=[
+          'Embarked_Q',
+          'SibSp',
+          'Age',
+          'Parch',
+          'Pclass_2'
+        ],
+        axis='columns',inplace=True)
+      transformed_df.append(df)
+
+    return tuple(transformed_df)
+
+  def scale(self, train_df, test_df):
+    scaler = self.scaler.fit(train_df)
+    return scaler.transform(train_df), \
+      scaler.transform(test_df)
+
+  def apply(self, train_df, test_df):
+    train_df, test_df = self.transform(
+      train_df, test_df
     )
-    df = pd.concat([df, pclass_dummy], axis=1)
-    df.drop('Pclass', axis=1, inplace=True)
-
-    sex_dummy = pd.get_dummies(df['Sex'])
-    df = pd.concat([df, sex_dummy], axis=1)
-    df.drop('Sex', axis=1, inplace=True)
-    return df
-
-  def transform(
-    self,
-    df: pd.DataFrame,
-  ) -> pd.DataFrame:
-    return self._scaler.transform(self.clean(df))
+    if self.scaler:
+      train_df, test_df = self.scale(
+        train_df, test_df
+      )
+    return train_df, test_df
